b8007cde619e52b3318d03bd579a3cea221f5f43
emi0528 <emillyg.gusmao06@gmail.com>
2025-08-05 11:36:04 -0300

    exercicio de phyton

---
+++ cofre.py
@@ -0,0 +1,14 @@
+import senha
+
+print(senha.minha_senha)
+
+
+import random
+
+numero_aleatorio = random.random()
+print(numero_aleatorio)
+
+import random 
+
+numero_aleatorio = random.randint(0,10)
+print(numero_aleatorio)
---
+++ senha.py
@@ -0,0 +1,7 @@
+minha_senha = "052818"
+def verificar_senhha(nova_senha):
+    if len(nova_senha) > 6:
+        print("Válida")
+    else:
+        print("Inválida")
+        
