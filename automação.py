e4ac35130461f3f20df84e7ce935c7bacf617df5
emi0528 <emillyg.gusmao06@gmail.com>
2025-09-02 11:01:32 -0300

    Add files via upload

---
+++ automação.py
@@ -0,0 +1,42 @@
+import mouseinfo, pyautogui
+
+#mouseinfo.mouseInfo()
+
+"""pyautogui.moveTo(1010,1058, duration=1)
+pyautogui.click()
+pyautogui.moveTo(452,70, duration=1)
+pyautogui.click()
+
+pyautogui.write("Barbie")
+pyautogui.press("Enter")
+"""
+#mouseinfo.mouseInfo()
+
+'''pyautogui.moveTo(1008,1046,duration=1)
+'pyautogui.moveTo
+pyautogui.moveTo(349,64, duration=1)
+pyautogui.click()
+pyautogui.write("estrela")
+pyautogui.write("estrela")
+'''
+
+'''pyautogui.moveTo(1017,1070, duration=1)
+pyautogui.click()
+pyautogui.moveTo(262,77, duration=1)
+pyautogui.click()
+
+pyautogui.write("jogos")
+pyautogui.press("Enter")
+'''
+
+
+
+
+# mouseinfo.mouseInfo()
+
+pyautogui.moveTo(1050,1058, duration=1)
+pyautogui.click()
+pyautogui.moveTo(603,72, duration=1)
+pyautogui.write("hello kitty")
+pyautogui.press("Enter")
+
